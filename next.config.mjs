184c60eb2324bfc12816e3bba68d86e095ab1e3f
melina <m.niavarani@hotmail.com>
2025-10-04 14:43:06 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,14 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   outputFileTracingRoot: __dirname, // Explicitly set the project root to silence lockfile warning
   reactStrictMode: false, // Reduces hydration warnings in development
-  async redirects() {
-    return [
-      {
-        source: '/en',
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
